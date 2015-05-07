51baf0a33731f86c288367819e41a7c5f512f4f2
IArgosyI-Lenovo <iargosyi@gmail.com>
2015-05-07 00:46:44 -0400

    animation on start screen added

---
+++ Assets/Scripts/Answers.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Answers : MonoBehaviour {
+    public int[] answers;
+	// Use this for initialization
+	void Start () {
+        //answers[0][0] = 0;
+        //answers[0][1] = 1;
+        //answers[0][2] = 2;
+        //answers[0][3] = 3;
+        //answers[0][4] = 4;
+        //answers[1][0] = 5;
+        //answers[1][1] = 1;
+        //answers[1][2] = 6;
+        //answers[1][3] = 7;
+        //answers[1][4] = 8;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAnimation : MonoBehaviour {
+    public int order;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(MoveToTheLeft());
+        StartCoroutine(JumpUpAndDown());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x > 560) transform.position -= new Vector3(590, 0, 0);
+	}
+
+    IEnumerator MoveToTheLeft()
+    {
+        while (true)
+        {
+            transform.position += new Vector3(0.5f, 0, 0);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    IEnumerator JumpUpAndDown()
+    {
+        yield return new WaitForSeconds(order*0.2f);
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            for (int i = 20; i > 0; i--)
+            {
+                transform.position += new Vector3(0, 2.0f*i/20, 0);
+                yield return new WaitForSeconds(0.01f);
+            }
+            for (int i = 1; i <= 20; i++)
+            {
+                transform.position += new Vector3(0, -2.0f*i / 20, 0);
+                yield return new WaitForSeconds(0.01f);
+            }
+            GetComponent<SpriteRenderer>().color = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
+            yield return new WaitForSeconds(0.4f);
+        }
+    }
+}
