eb357ef9e5c674dfa80fc6a9aeaf46ca6aff2eb5
IArgosyI-Lenovo <iargosyi@gmail.com>
2015-05-07 10:55:11 -0400

    Fix a bunch of things

--- Assets/Scripts/ColorOrb.cs
+++ Assets/Scripts/ColorOrb.cs
@@ -67,7 +67,11 @@ public class ColorOrb : MonoBehaviour {
     void OnMouseDown()
     {
         //Debug.Log("?");
-        if(mainGame.animationDone && mainGame.playing)mainGame.EatColorOrb(this);
+        if (mainGame.animationDone && mainGame.playing)
+        {
+            GetComponent<AudioSource>().Play();
+            mainGame.EatColorOrb(this);
+        }
     }
 
     public IEnumerator ColorAnimation()
